1344acbe6b00d40300a6f3444c83f0b3832c3aa4
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-10 10:34:42 +0700

    add metadata to each page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 import '@/app/globals.css';
+import { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
 const roboto = Roboto();
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Basic Todo',
+    default: 'Basic Todo'
+  },
+  description: 'World No.1 Todo List Web Application'
+};
 
 export default function RootLayout({
   children
